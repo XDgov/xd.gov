935087e77d44089edb4ff30bf1733826e2d18e47
curt-mitch-census <curtis.l.mitchell@census.gov>
2023-12-22 11:08:32 -0800

    Add cache for sharp image creation

--- .github/sharp/sharp.js
+++ .github/sharp/sharp.js
@@ -2,6 +2,7 @@ const sharp = require('sharp');
 const fs = require('fs');
 const path = require('path');
 
+const { readFile, writeFile } = fs.promises;
 const { getFileListOfTypes } = require('../../helpers/utilities');
 
 
@@ -14,6 +15,7 @@ const IMAGE_SIZES = [
   ['xxl', 2500],
 ];
 const assetsImagesDir = './assets/img';
+const imageCacheFilePath = './.github/sharp/image-cache.json';
 const imgDirectories = [
   `${assetsImagesDir}/heroes`,
   `${assetsImagesDir}/import`,
@@ -23,7 +25,28 @@ const imgDirectories = [
   `${assetsImagesDir}/praise`,
 ];
 
-function makeResponsiveImages(imagesList) {
+async function retrieveCache() {
+  const cacheFile = await readFile(imageCacheFilePath, 'utf8');
+  const cache = JSON.parse(cacheFile);
+
+  return cache;
+}
+
+const imageInCache = (imagePath, cache) => cache[imagePath];
+
+const addImageToCache = (imagePath, cache) => (cache[imagePath] = true);
+
+async function saveImageCacheFile(cache) {
+  return await writeFile(imageCacheFilePath, JSON.stringify(cache), (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('Image cache updated');
+  });
+}
+
+function makeResponsiveImages(imagesList, cache) {
   imagesList.map((imgDir) => {
     const sharpFile = sharp(imgDir);
     const filepath = path.parse(imgDir).dir;
@@ -37,18 +60,34 @@ function makeResponsiveImages(imagesList) {
       sharpFile
         .resize(newWidth) // sharp module will maintain aspect ratio
         .toFile(`${filepath}/${filename}-${sizeLabel}${fileExtension}`);
+
+      addImageToCache(`${filepath}/${filename}-${sizeLabel}${fileExtension}`, cache);
     }
 
     // create webp version of original image
     sharpFile
       .toFormat('webp')
       .toFile(`${filepath}/${filename}.webp`)
+
+    addImageToCache(`${filepath}/${filename}.webp`, cache);
   });
 }
 
+async function checkAndUpdateImages(cache) {
+  return imgDirectories.map(async (dir) => {
+    let currentImages = await getFileListOfTypes(dir, SHARP_FORMATS);
 
-imgDirectories.map(async (dir) => {
-  const currentImages = await getFileListOfTypes(dir, SHARP_FORMATS);
-  // TODO: create image cache to avoid duplicate processing
-  makeResponsiveImages(currentImages);
-});
+    // ensure responsive images are created for new images only
+    currentImages = currentImages.filter((image) => !imageInCache(image, cache));
+    makeResponsiveImages(currentImages, cache);
+    currentImages.map((image) => addImageToCache(image, cache));
+  });
+}
+
+// TODO: add templating for responsive and webp images in markdown files
+(async () => {
+  const cache = await retrieveCache();
+
+  await Promise.all(await checkAndUpdateImages(cache));
+  await saveImageCacheFile(cache);
+})();
