3dbcc23349fe62cc3106f18fcdf7a84aa45e052d
Curtis Mitchell <curtis.l.mitchell@census.gov>
2024-04-12 13:03:24 -0700

    JS file for AJAX post; minor form cleanup

---
+++ helpers/applicationFormSubmission.js
@@ -0,0 +1,38 @@
+const form = document.getElementById("application-form");
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+	event.preventDefault(); // prevent page from reloading on submit
+
+    const data = new FormData(event.target);
+    const dataObject = Object.fromEntries(data.entries());
+
+    // postAjax('census-application-submit.gov', dataObject, onSubmitSuccess);
+}
+
+function onSubmitSuccess(responseText) {
+    console.log(responseText);
+    form.reset();
+}
+
+
+
+function postAjax(url, data, success) {
+    var params = typeof data == 'string' ? data : Object.keys(data).map(
+            function(k){ return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) }
+        ).join('&');
+
+    var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+    xhr.open('POST', url);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState>3 && xhr.status==200) { success(xhr.responseText); }
+    };
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(params);
+    return xhr;
+}
+
+// example request with data object
+// postAjax('http://foo.bar/', { p1: 1, p2: 'Hello World' }, function(data){ console.log(data); });
